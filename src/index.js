e808be2192e9ac62bd7abc71fca6785354e953e9
Anton <deftume@google.com>
2022-07-04 00:13:04 +0300

    feat: the solution of the problem reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  n = Math.abs(n).toString();
+  let reverseNum = ""
+  for(let i = n.length-1; i>=0; i--) {
+    reverseNum += n[i];
+  }
+  return reverseNum;
 }
